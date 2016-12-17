2f70625d6265744756f16b935e447fb0344c2f20
rike307 <henrique.paulo307@hotmail.com>
2016-12-16 22:31:55 -0200

    aula 6 parada

---
+++ Quiron.LojaVitual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Quiron.LojaVitual.Dominio.Entidade;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVitual.Dominio.Repositorio
+{
+   public class EfDbContext : DbContext{
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+}
